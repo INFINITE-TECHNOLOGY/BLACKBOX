6d397b84961a8bc3814c54eb2df07b29d668fd7a
apryamostanov <anton.pryamostanov@gmail.com>
2018-11-01 15:03:08 +0400

    Preliminary AnonymousClass test (work in progress)

---
+++ src/test/groovy/io/infinite/AnonymousClass.groovy
@@ -0,0 +1,38 @@
+package io.infinite
+
+import io.infinite.blackbox.BlackBox
+import io.infinite.blackbox.BlackBoxLevel
+
+class AnonymousClass<Type> extends ArrayList<Type> {
+
+    private Integer index = 0
+
+    @BlackBox(blackBoxLevel = BlackBoxLevel.EXPRESSION)
+    @Override
+    Iterator<Type> iterator() {
+        return new Iterator<Type>() {
+
+            //@BlackBox(blackBoxLevel = io.infinite.blackbox.BlackBoxLevel.EXPRESSION)
+            @Override
+            boolean hasNext() {
+                return true
+            }
+
+            //@BlackBox(blackBoxLevel = io.infinite.blackbox.BlackBoxLevel.EXPRESSION)
+            @Override
+            Type next() {
+                Type result = get(index)
+                index = ((index + 1) % size())
+                return result
+            }
+
+            //@BlackBox(blackBoxLevel = io.infinite.blackbox.BlackBoxLevel.EXPRESSION)
+            @Override
+            void remove() {
+                throw new Exception("Unable to remove from RoundRobin")
+            }
+
+        }
+    }
+
+}
