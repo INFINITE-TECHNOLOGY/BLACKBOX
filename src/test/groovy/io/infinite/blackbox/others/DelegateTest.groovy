f68dcc60e9094f4cf8c30e8217a6682122d3a13b
apryamostanov <anton.pryamostanov@gmail.com>
2018-11-22 11:33:36 +0400

    More tests for #2

--- src/test/groovy/io/infinite/blackbox/others/DelegateTest.groovy
+++ src/test/groovy/io/infinite/blackbox/others/DelegateTest.groovy
@@ -5,15 +5,24 @@ import io.infinite.blackbox.BlackBoxLevel
 
 class DelegateTest {
 
-
     @BlackBox(blackBoxLevel = BlackBoxLevel.EXPRESSION)
-    void test() {
+    void testBlackBox(Object iThis) {
         Closure c = {
             assert delegate == this
             assert owner == this
             assert thisObject == this
+            assert delegate == iThis
+            assert owner == iThis
+            assert thisObject == iThis
+            assert iThis == this
+            assert iThis == this
+            assert iThis == this
         }
         c()
+    }
+
+    void test() {
+        testBlackBox(this)
     }
 
 }
