0f93458296287d04ccae623c28aef23ab474ddc2
apryamostanov <anton.pryamostanov@gmail.com>
2019-02-20 17:32:14 +0400

    Fix for "Error while popping argument from operand stack tracker in class io/infinite/pigeon/http/SenderAWS method void <clinit>()."

--- src/main/groovy/io/infinite/blackbox/BlackBoxEngine.groovy
+++ src/main/groovy/io/infinite/blackbox/BlackBoxEngine.groovy
@@ -24,6 +24,9 @@ class BlackBoxEngine extends CarburetorEngine {
 
     Logger internalLogger
 
+    BlackBoxEngine() {
+    }
+
     BlackBoxEngine(Logger internalLogger) {
         this.internalLogger = internalLogger
     }
---
+++ src/test/groovy/io/infinite/blackbox/AbstractTest.groovy
@@ -0,0 +1,30 @@
+package io.infinite.blackbox
+
+import groovy.transform.Memoized
+import org.slf4j.MDC
+
+abstract class AbstractTest extends GroovyTestCase {
+
+    GroovyClassLoader groovyClassLoader = new GroovyClassLoader()
+
+    void test() {
+        executeTests()
+    }
+
+    abstract void executeTests()
+
+    @Memoized
+    def getTestInstance(String sectionName, String testScriptName) {
+        def testInstance = groovyClassLoader.parseClass(getTestScript(sectionName, testScriptName)).newInstance()
+        MDC.put("unitName", "EXECUTION_${testScriptName}")
+        return testInstance
+    }
+
+    File getTestScript(String sectionName, String testScriptName) {
+        ClassLoader classLoader = getClass().getClassLoader()
+        File file = new File(classLoader.getResource(sectionName + "/" + testScriptName).getFile())
+        return file
+    }
+
+}
+
